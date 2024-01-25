b382e12166696173f6b773784f80d7cf1c62431f
AbdouBouj <abderrahimboujedar@gmail.com>
2024-01-25 11:40:10 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Login } from '@mui/icons-material'
 import './App.css'
 import Events from './components/Events/Events'
 import Header from './components/Header/Header'
+import LoginForm from './components/LoginForm/LoginForm'
 import Speakers from './components/Speakers/Speakers'
 import {Routes,Route} from "react-router-dom"
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Events/>} />
       <Route path="/speakers" element={<Speakers/>} />
-      <Route path="/login" element={<Login/>} />
+      <Route path="/login" element={<LoginForm/>} />
     </Routes>
     </>
   )
