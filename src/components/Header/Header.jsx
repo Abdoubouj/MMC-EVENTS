4e20d7fa25ed23ff8a00b6b1add13db647992df0
AbdouBouj <abderrahimboujedar@gmail.com>
2024-01-25 15:59:29 +0100

    add login and register Links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,68 +1,81 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Header.scss";
 import mmcLogo from "../../assets/mmcLogo.svg";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 const Header = () => {
   const headerTopRef = useRef(null);
+  const [showAccount, setShowAccount] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 100) {
-        headerTopRef.current.style.boxShadow = 'rgba(0, 0, 0, 0.1) 0px 4px 12px';
+        headerTopRef.current.style.boxShadow =
+          "rgba(0, 0, 0, 0.1) 0px 4px 12px";
       } else {
-        headerTopRef.current.style.boxShadow = 'none';
+        headerTopRef.current.style.boxShadow = "none";
       }
     };
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   return (
     <header className="header" ref={headerTopRef}>
       <div className="container">
         <div className="header-top">
-            <div className="logo">
-              <img src={mmcLogo} width={90} alt="app-logo" />
-            </div>
-            <div className="menu">
-              <ul className="nav-menu">
-                <li className="menu-item">
-                  <NavLink className="menu-link" to="/">
-                    Home
-                  </NavLink>
-                </li>
-                <li className="menu-item">
-                  <NavLink className="menu-link" to="/">
-                    Events
-                  </NavLink>
-                </li>
-                <li className="menu-item">
-                  <NavLink className="menu-link" to="/speakers">
-                    Speakers
-                  </NavLink>
-                </li>
-                <li className="menu-item">
-                  <NavLink className="menu-link" to="/">
-                    About Us
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-            <div className="header-left">
-              <div className="search-box">
-                <input type="search" placeholder="search for Event ..." />
-                <div className="search-icon">
-                  <SearchRoundedIcon />
-                </div>
-              </div>
-              <div className="account-box">
-                <div className="account-icon">
-                  <PersonOutlineIcon />
-                </div>
+          <div className="logo">
+            <img src={mmcLogo} width={60} alt="app-logo" />
+          </div>
+          <div className="menu">
+            <ul className="nav-menu">
+              <li className="menu-item">
+                <NavLink className="menu-link" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="menu-item">
+                <NavLink className="menu-link" to="/">
+                  Events
+                </NavLink>
+              </li>
+              <li className="menu-item">
+                <NavLink className="menu-link" to="/speakers">
+                  Speakers
+                </NavLink>
+              </li>
+              <li className="menu-item">
+                <NavLink className="menu-link" to="/">
+                  About Us
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="header-left">
+            <div className="search-box">
+              <input type="search" placeholder="search for Event ..." />
+              <div className="search-icon">
+                <SearchRoundedIcon />
               </div>
             </div>
+            <div className="account-box">
+              <button
+                className="account-icon"
+                onClick={() => {
+                  setShowAccount(!showAccount);
+                }}
+              >
+                <PersonOutlineIcon />
+              </button>
+              {showAccount && (
+                <div className="auth-box">
+                  <Link className="btn login-btn" to="/login">login</Link>
+                  <Link className="btn register-btn" to="/register">register</Link>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </header>
