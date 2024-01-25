ffda3b36dbeecf92fd2aa1c4bc1e334682ab60dc
AbdouBouj <abderrahimboujedar@gmail.com>
2024-01-25 10:09:10 +0100

    create events component

---
+++ src/components/Events/Events.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Events.scss"
+const Events = () => {
+  return (
+    <section className='events_section'>
+        <div className="events_container">
+        <h1>Latest Events</h1>
+        </div>
+    </section>
+  )
+}
+
+export default Events
