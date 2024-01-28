f952e68689152c86963159a60e6c9db0b3268cd7
AbdouBouj <abderrahimboujedar@gmail.com>
2024-01-28 18:52:20 +0100

    update

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import "./AdminDashboard.scss"
+import mmcLogo from "../../assets/mmcLogo.svg"
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <div className="admin-dashboard">
+        <aside className="dashboard-sidebare">
+           <div className="logo">
+            <img className='logo-img' src={mmcLogo} width={90} alt="#" />
+            <h1 className='logo-title'>Morocco Microsoft Community</h1>
+           </div>
+           <ul className="dashboard-menu">
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">Users</a></li>
+                <li><a href="#">Speakers</a></li>
+                <li><a href="#">Partners</a></li>
+           </ul>
+        </aside>
+    </div>
   )
 }
 
