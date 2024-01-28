6b1397c884c03c299301b7350c66c402344a4b49
AbdouBouj <abderrahimboujedar@gmail.com>
2024-01-28 19:58:38 +0100

    update dashboard menu

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import "./AdminDashboard.scss"
 import mmcLogo from "../../assets/mmcLogo.svg"
+import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
+import ConfirmationNumberRoundedIcon from '@mui/icons-material/ConfirmationNumberRounded';
+import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
+import RecordVoiceOverRoundedIcon from '@mui/icons-material/RecordVoiceOverRounded';
+import HandshakeRoundedIcon from '@mui/icons-material/HandshakeRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 const AdminDashboard = () => {
   return (
     <div className="admin-dashboard">
@@ -10,11 +16,12 @@ const AdminDashboard = () => {
             <h1 className='logo-title'>Morocco Microsoft Community</h1>
            </div>
            <ul className="dashboard-menu">
-                <li><a href="#">Dashboard</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Users</a></li>
-                <li><a href="#">Speakers</a></li>
-                <li><a href="#">Partners</a></li>
+                <li><a href="#"><GridViewRoundedIcon/> Dashboard</a></li>
+                <li><a href="#"><ConfirmationNumberRoundedIcon/> Events</a></li>
+                <li><a href="#"><PeopleRoundedIcon/> Users</a></li>
+                <li><a href="#"><RecordVoiceOverRoundedIcon/> Speakers</a></li>
+                <li><a href="#"><HandshakeRoundedIcon/> Partners</a></li>
+                <li><a href="#"><LogoutRoundedIcon/> Logout</a></li>
            </ul>
         </aside>
     </div>
