7c176ec4f48645505612f7fb8fb52bf5c98889f3
anassbg <bgmza.anass@gmail.com>
2024-01-29 09:41:18 +0100

    update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,36 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './LoginForm.css'
+import { Link } from "react-router-dom";
+import vector from "../../assets/vector.svg";
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
 
-
-export default function LoginForm() {
-
-   
-
-
+export default function LoginForm() {   
 
     return (
-        <div className='outerdiv'>
-            <form className="form">
-                <span className="signup">Sign In</span>
-                <input type="name" placeholder="User name" className="form--input" name="userName"  />
-                <input type="password" placeholder="Password" className="form--input" name="password"  />
-                <div className="form--marketing">
-                    <div style={{ marginRight: "15rem" }}>
-                        <input id="okayToEmail" type="checkbox" />
-                        <label htmlFor="okayToEmail" className="checkbox">
-                            Remember me
-                        </label>
-                    </div>
-                    <a href="">Forgot Password</a>
-                </div>
-
-                <div className='buttons-div'>
-                    <button className="form--submit" >
-                        Login
-                    </button>
-                </div>
-            </form>
+        <section className="login-section">
+        <div className="left">
+          <img src={vector} alt="#" />
         </div>
+        <div className="right">
+          <div className="form-container">
+            <h1>Sign in</h1>
+            <form action="">
+              <div className="field form-email">
+                <label htmlFor="email">email</label>
+                <div className="emailInput">
+                <input type="text" placeholder="email" />
+                <div className="emailIcon">
+                <EmailOutlinedIcon/>
+                </div>
+                </div>
+              </div>
+              <div className="field form-password">
+                <label htmlFor="password">password</label>
+                <div className="passwordInput">
+                <input type="password" placeholder="password" />
+                <div className="passwordIcon">
+                <VpnKeyOutlinedIcon/>
+                </div>
+                </div>
+              </div>
+              <button className="btn create-account-btn">sign in</button>
+              <p>
+              Don't have account ?
+              <Link to="/register">Register</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </section>
     );
 }
 
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,25 +1,59 @@
-import React, { useState } from 'react';
-import "../LoginForm/LoginForm.css"
+import React from 'react';
+import "./RegisterForm.css"
+import { Link } from 'react-router-dom';
+import vector from "../../assets/vector.svg";
+
+
 export default function RegisterForm() {   
     return (
-        <div className='outerdiv'>
-            <form className="form">
-                <span className="signup">Sign Up</span>
-                <input type="username" placeholder="User name" className="form--input" name="userName" />
-                <input type="email" placeholder="Email address" className="form--input" name="email"  />
-                <input type="tel" placeholder="Contact Number" className="form--input" maxLength="10" name="phoneNumber"  />
-                <input type="password" placeholder="Password" className="form--input" name="password"   />
-                <input type="password" placeholder="Confirm password" className="form--input" name="confirmPasword"  />
-
-                <div className='buttons-div'>
-                    <button className="form--submit" >
-                        Save & Verify
-                    </button>
-                </div>
-
-              
-            </form>
-
+        <section className="register-section">
+        <div className="left">
+          <img src={vector} alt="#" />
         </div>
+        <div className="right">
+          <div className="form-container">
+            <h1>Create an account</h1>
+            <form action="">
+              <div className="field form-prenom">
+                <label htmlFor="prenom">prenom</label>
+                <input type="text" placeholder="prenom" />
+              </div>
+              <div className="field form-nom">
+                <label htmlFor="nom">nom</label>
+                <input type="text" placeholder="nom" />
+              </div>
+              <div className="field form-adresse">
+                <label htmlFor="adresse">adresse</label>
+                <input type="text" placeholder="adresse" />
+              </div>
+              <div className="field form-sexe">
+                <label htmlFor="sexe">sexe</label>
+                <select name="sexe" id="sexe">
+                    <option value="Men">Men</option>
+                    <option value="Women">Women</option>
+                </select>
+              </div>
+              <div className="field form-GSM">
+                <label htmlFor="GSM">GSM</label>
+                <input type="text" placeholder="GSM" />
+              </div>
+              <div className="field form-email">
+                <label htmlFor="email">email</label>
+                <input type="text" placeholder="email" />
+              </div>
+              <div className="field form-password">
+                <label htmlFor="password">password</label>
+                <input type="password" placeholder="password" />
+              </div>
+              <div className="field form-confirm-password">
+                <label htmlFor="confirm-password">confirm password</label>
+                <input type="password" placeholder="password" />
+              </div>
+              <button className="btn create-account-btn">create account</button>
+              <Link className="btn sign-in-btn" to="/login">sign in</Link>
+            </form>
+          </div>
+        </div>
+      </section>
     );
 }
